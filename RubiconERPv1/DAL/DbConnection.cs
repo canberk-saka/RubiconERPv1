de93932c6347abf088244a0d7d40e36f47357003
Emre Al <emre_al@hotmail.com>
2024-12-28 22:15:21 +0300

    en son güncellemeler

--- RubiconERPv1/DAL/DbConnection.cs
+++ RubiconERPv1/DAL/DbConnection.cs
@@ -7,7 +7,7 @@ namespace DataAccessLayer
     {
 
         // Veritabanı bağlantı dizesi
-        private static readonly string _connectionString = "Data Source=DESKTOP-BAP4RDU\\SQLEXPRESS02;Initial Catalog=RubiconDB;Integrated Security=True;";
+        private static readonly string _connectionString = "Data Source=EMRE;Initial Catalog=RubiconDB;Integrated Security=True;";
 
 
         // SqlConnection döndüren bir metot
