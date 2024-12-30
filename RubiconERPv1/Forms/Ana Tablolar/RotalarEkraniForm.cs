c4d29e47d32b6092fe278b26180c6f95a319ec98
Muhammed Emin Canbaz <emincanbazk@gmail.com>
2024-12-31 02:07:50 +0300

    guncel hali cektigimin commiti

---
+++ RubiconERPv1/Forms/Ana Tablolar/RotalarEkraniForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RubiconERPv1.Forms.Ana_Tablolar
+{
+    public partial class RotalarEkraniForm : Form
+    {
+        public RotalarEkraniForm()
+        {
+            InitializeComponent();
+        }
+
+        private void RotalarEkraniForm_Load(object sender, EventArgs e)
+        {
+            // Ekran boyutlarını al
+            this.Size = Screen.PrimaryScreen.Bounds.Size;
+
+            // Formun konumunu ekranın köşesine ayarla
+            this.Location = Screen.PrimaryScreen.Bounds.Location;
+        }
+    }
+}
