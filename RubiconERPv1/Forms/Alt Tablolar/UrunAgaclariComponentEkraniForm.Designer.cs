12ca36a4d268b0c2f4ad8486058df98704f2636a
Muhammed Emin Canbaz <emincanbazk@gmail.com>
2024-12-30 15:58:34 +0300

    son halindeyim

--- RubiconERPv1/Forms/Alt Tablolar/UrunAgaclariComponentEkraniForm.Designer.cs
+++ RubiconERPv1/Forms/Alt Tablolar/UrunAgaclariComponentEkraniForm.Designer.cs
@@ -51,10 +51,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Font = new System.Drawing.Font("MS Reference Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.label1.Location = new System.Drawing.Point(38, 64);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(238, 25);
+            this.label1.Size = new System.Drawing.Size(299, 26);
             this.label1.TabIndex = 1;
             this.label1.Text = "Ürün Ağacı Bileşen Listesi";
             this.label1.Click += new System.EventHandler(this.label1_Click);
