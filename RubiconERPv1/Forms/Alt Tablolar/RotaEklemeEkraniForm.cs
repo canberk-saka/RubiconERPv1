d7cd83dd7f02e9100794730cbebe965fcf1a2ec9
Muhammed Emin Canbaz <emincanbazk@gmail.com>
2025-01-01 15:36:48 +0300

    sadece fonksiyonun yerini degistirdim

--- RubiconERPv1/Forms/Alt Tablolar/RotaEklemeEkraniForm.cs
+++ RubiconERPv1/Forms/Alt Tablolar/RotaEklemeEkraniForm.cs
@@ -19,11 +19,11 @@ namespace RubiconERPv1.Forms.Alt_Tablolar
 
         private void RotaEklemeEkraniForm_Load(object sender, EventArgs e)
         {
-
-        }// Ekran boyutlarını al
+            // Ekran boyutlarını al
             this.Size = Screen.PrimaryScreen.Bounds.Size;
 
             // Formun konumunu ekranın köşesine ayarla
             this.Location = Screen.PrimaryScreen.Bounds.Location;
+        }
     }
 }
